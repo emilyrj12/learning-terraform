3267b94795d4b4b476828e3881b0125d7fc26ca8
emilyrj12 <emilyjose1597@gmail.com>
2023-10-11 11:27:19 +0100

    added helm resource file

--- main.tf
+++ main.tf
@@ -17,3 +17,15 @@ resource "kubernetes_namespace" "jenkins" {
     name = "jenkins"
   }
 }
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
